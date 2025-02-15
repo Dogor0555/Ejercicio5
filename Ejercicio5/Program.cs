d244695f506c38b9abdf61d6386cf2c3fb03a073
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 05:45:37 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.Write("Ingrese el primer número: ");
+string input1 = Console.ReadLine();
+
+Console.Write("Ingrese el segundo número: ");
+string input2 = Console.ReadLine();
+
+int num1 = int.Parse(input1);
+int num2 = int.Parse(input2);
+
+int suma = num1 + num2;
+
+Console.WriteLine("La suma de los dos números es: " + suma);
